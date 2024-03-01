c4ba51bb3d3d62928053199b3382cd509f9fea62
Satya Kaul <satya.kaul@eviden.com>
2024-03-01 19:07:47 +0530

    updates to new sections

--- src/components/Maincontent.js
+++ src/components/Maincontent.js
@@ -1,13 +1,33 @@
+import React, { useState } from 'react'
 import Employee from './Employee'
 import Hello from './Hello'
 import World from './World'
+import PostList from './PostList'
+import PhotoList from './PhotoList'
 
 const Maincontent = () => {
+  const [showResults, setShowResults] = useState(true)
+
+  const onClickHandler = () => {
+    setShowResults(true)
+  }
+
+  const onClickPhotoHandler = () => {
+    setShowResults(false)
+  }
+
   return (
     <div>
       <Hello />
       <World />
       <Employee />
+      <button class='btn btn-primary btn-lg' onClick={onClickHandler}>
+        Get Data
+      </button>
+      <button class='btn btn-primary btn-lg' onClick={onClickPhotoHandler}>
+        Get Photo
+      </button>
+      {showResults ? <PostList/> : <PhotoList/>}
     </div>
   )
 }
